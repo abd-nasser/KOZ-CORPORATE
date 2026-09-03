9a3e246bbff05d0e7a054d261bb88283c2c96f03
abd-nasser <nasabsas@gmail.com>
2026-09-03 10:54:08 +0000

    debug false

--- src/koz_flow/settings.py
+++ src/koz_flow/settings.py
@@ -163,15 +163,15 @@ WSGI_APPLICATION = 'koz_flow.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-if DEBUG:
+"""if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR /"db" /'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
+else:"""
+DATABASES = {
             'default': {
                     'ENGINE': 'django.db.backends.postgresql',
                     'NAME': os.getenv('POSTGRES_DB'),
